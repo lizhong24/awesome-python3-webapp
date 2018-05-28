6bbebd4b9659f6b1a4416ac7dcdeb58594a9101c
lizhong24 <lang812>
2018-05-28 21:44:28 +0800

    编写Model

---
+++ www/testdb.py
@@ -0,0 +1,17 @@
+import orm
+from models import User, Blog, Comment
+import asyncio
+
+
+async def test():
+    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='root', db='awesome')
+
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+
+    await u.save()
+
+# 获取EventLoop:
+loop = asyncio.get_event_loop()
+
+# 把协程丢到EventLoop中执行
+loop.run_until_complete(test())
